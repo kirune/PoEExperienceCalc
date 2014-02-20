11b3db5dcea1b7506da22fea81ce8278fedff543
RobB <kaeirlic@hotmail.com>
2014-02-20 11:10:32 -0800

    Changed form name and changed formatting of output

--- PoE Experience Calc/PoE Experience Calc.Designer.cs
+++ PoE Experience Calc/PoE Experience Calc.Designer.cs
@@ -33,9 +33,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.MonsterLevel = new System.Windows.Forms.TextBox();
             this.Calc = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.Results = new System.Windows.Forms.TextBox();
             this.ExperienceRange = new System.Windows.Forms.RichTextBox();
+            this.Reset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // PlayerLevel
@@ -74,7 +73,7 @@
             // 
             // Calc
             // 
-            this.Calc.Location = new System.Drawing.Point(111, 111);
+            this.Calc.Location = new System.Drawing.Point(40, 122);
             this.Calc.Name = "Calc";
             this.Calc.Size = new System.Drawing.Size(75, 23);
             this.Calc.TabIndex = 2;
@@ -82,36 +81,26 @@
             this.Calc.UseVisualStyleBackColor = true;
             this.Calc.Click += new System.EventHandler(this.Calc_Click);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(230, 53);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Experience Gain";
-            // 
-            // Results
-            // 
-            this.Results.BackColor = System.Drawing.Color.Silver;
-            this.Results.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Results.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.Results.Location = new System.Drawing.Point(321, 53);
-            this.Results.Name = "Results";
-            this.Results.Size = new System.Drawing.Size(100, 13);
-            this.Results.TabIndex = 4;
-            this.Results.TabStop = false;
-            // 
             // ExperienceRange
             // 
             this.ExperienceRange.BackColor = System.Drawing.Color.Silver;
             this.ExperienceRange.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.ExperienceRange.Location = new System.Drawing.Point(40, 151);
+            this.ExperienceRange.Location = new System.Drawing.Point(40, 166);
             this.ExperienceRange.Name = "ExperienceRange";
-            this.ExperienceRange.Size = new System.Drawing.Size(361, 33);
+            this.ExperienceRange.Size = new System.Drawing.Size(338, 45);
             this.ExperienceRange.TabIndex = 5;
             this.ExperienceRange.TabStop = false;
             this.ExperienceRange.Text = "";
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(136, 122);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 6;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
             // 
             // Form1
             // 
@@ -119,10 +108,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(442, 196);
+            this.ClientSize = new System.Drawing.Size(411, 223);
+            this.Controls.Add(this.Reset);
             this.Controls.Add(this.ExperienceRange);
-            this.Controls.Add(this.Results);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.Calc);
             this.Controls.Add(this.MonsterLevel);
             this.Controls.Add(this.label2);
@@ -130,7 +118,7 @@
             this.Controls.Add(this.PlayerLevel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "PoE Experience Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,9 +131,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox MonsterLevel;
         private System.Windows.Forms.Button Calc;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox Results;
         private System.Windows.Forms.RichTextBox ExperienceRange;
+        private System.Windows.Forms.Button Reset;
     }
 }
 
--- PoE Experience Calc/PoE Experience Calc.cs
+++ PoE Experience Calc/PoE Experience Calc.cs
@@ -15,6 +15,7 @@ namespace PoE_Experience_Calc
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -59,37 +60,37 @@ namespace PoE_Experience_Calc
                 if (MonsterLevelNum < LowerLevel)
                 {
                     ExperienceRange.Text = "";
-                    Results.Text = "";
+                    //Results.Text = "";
                     EffectiveDifference = LowerLevel - MonsterLevelNum;
                     Experience = Math.Pow((PlayerLevelNum + 5) / (PlayerLevelNum + 5 + Math.Pow(EffectiveDifference, 2.5)), 1.5) * 100;
                     FinalExperience = Math.Round(Experience);
                     if (FinalExperience < 2)
                     { FinalExperience = 2; }
-                    Results.Text = FinalExperience.ToString() + "%";
-                    ExperienceRange.Text = "You will gain 100% experience from killing monsters level " + LowerLevel + " - " + TopLevel;
+                    ///Results.Text = FinalExperience.ToString() + "%";
+                    ExperienceRange.Text = "You get " + FinalExperience + "% experience for killing monsters level " + MonsterLevelNum + ". \r\n \r\nYou will gain 100% experience from killing monsters level " + LowerLevel + " - " + TopLevel;
                 }
 
                 else if (MonsterLevelNum > TopLevel)
                 {
                     ExperienceRange.Text = "";
-                    Results.Text = "";
+                    ///Results.Text = "";
                     EffectiveDifference = MonsterLevelNum - TopLevel;
                     Experience = Math.Pow((PlayerLevelNum + 5) / (PlayerLevelNum + 5 + Math.Pow(EffectiveDifference, 2.5)), 1.5) * 100;
                     FinalExperience = Math.Round(Experience);
                     if (FinalExperience < 2)
                     { FinalExperience = 2; }
-                    Results.Text = FinalExperience.ToString() + "%";
-                    ExperienceRange.Text = "You will gain 100% experience from killing monsters level " + LowerLevel + " - " + TopLevel;
+                    ///Results.Text = FinalExperience.ToString() + "%";
+                    ExperienceRange.Text = "You get " + FinalExperience + "% experience for killing monsters level " + MonsterLevelNum + ". \r\n \r\nYou will gain 100% experience from killing monsters level " + LowerLevel + " - " + TopLevel;
                 }
                 else if (MonsterLevelNum >= LowerLevel && MonsterLevelNum <= TopLevel)
                 {
                     ExperienceRange.Text = "";
-                    Results.Text = "";
+                    ///Results.Text = "";
                     EffectiveDifference = 0;
                     Experience = Math.Pow((PlayerLevelNum + 5) / (PlayerLevelNum + 5 + Math.Pow(EffectiveDifference, 2.5)), 1.5) * 100;
                     FinalExperience = Math.Round(Experience);
-                    Results.Text = FinalExperience.ToString() + "%";
-                    ExperienceRange.Text = "You will gain 100% experience from killing monsters level " + LowerLevel + " - " + TopLevel;
+                    ///Results.Text = FinalExperience.ToString() + "%";
+                    ExperienceRange.Text = "You get " + FinalExperience + "% experience for killing monsters level " + MonsterLevelNum + ". \r\n \r\nYou will gain 100% experience from killing monsters level " + LowerLevel + " - " + TopLevel;
                 }
 
             }
@@ -97,12 +98,14 @@ namespace PoE_Experience_Calc
 
         }
 
-            
-
-           
-
-           
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            MonsterLevel.Text = "";
+            PlayerLevel.Text = "";
         }
+
+       
+    }
 
      
      
